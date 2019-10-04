ab9f5cb01c9eb5b3efb1ebdd1ec936da84c93ba3
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-10-03 18:45:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 console.warn("This will overwrite your W93 firmware!")
-dont_kill_your_files = prompt("This will overwrite your W93 firmware! Are you sure you want to continue? Enter \"yEs\" to continue.")
+dont_kill_your_files = $prompt("This will overwrite your W93 firmware! Are you sure you want to continue? Enter \"yEs\" to continue.")
 
 if (dont_kill_your_files == "yEs") {
-  console.log("Installing JSBoot...")
+  $alert("Installing JSBoot...")
 } else {
   window.location.reload(true)
 }
