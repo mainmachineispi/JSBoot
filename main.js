0f6ac34b88ff59b4c368fe0e0020a3fd3f89dcf5
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-10-03 18:41:50 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+console.warn("This will overwrite your W93 firmware!")
+dont_kill_your_files = prompt("This will overwrite your W93 firmware! Are you sure you want to continue? Enter \"yEs\" to continue.")
+
+if (dont_kill_your_files == "yEs") {
+  console.log("Installing JSBoot...")
+} else {
+  window.location.reload(true)
+}
+
+$window("https://github.com/mainmachineispi/JSBoot")
