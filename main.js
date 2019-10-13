f6448f1fe9f47fb29b0f8e9e83eafc86b8eb573a
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-10-12 19:10:50 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 console.warn("This will overwrite your W93 firmware!")
-dont_kill_your_files = $alert("This will overwrite your W93 firmware!")
+$alert("This will overwrite your W93 firmware!")
 
 $.ajax({
     url:'https://raw.githubusercontent.com/mainmachineispi/JSBoot/master/hijack.js',
