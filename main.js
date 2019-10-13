97ee8771d8f6cb12016ad08a7082dbc4f26f74af
mainmachineispi <54458505+mainmachineispi@users.noreply.github.com>
2019-10-12 19:22:54 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 console.warn("This will overwrite your W93 firmware!")
 $alert("This will overwrite your W93 firmware!")
 
-$.ajax({
+$ajax({
     url:'https://raw.githubusercontent.com/mainmachineispi/JSBoot/master/hijack.js',
     success: function (data){
       $db.set("boot/JSBoot_DONT_DELETE.js", data)
